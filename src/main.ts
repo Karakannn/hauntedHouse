5ea8188f8449360d13a59e6ec49c0aed4c00c202
Karakannn <eminkarakan0@gmail.com>
2022-10-15 17:55:08 +0200

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -23,13 +23,13 @@ const clock = new Clock();
 //Textures#
 
 //Door
-const doorColorTexture = textureLoader.load('../textures/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('../textures/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('../textures/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('../textures/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('../textures/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('../textures/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('../textures/textures/door/roughness.jpg')
+const doorColorTexture = textureLoader.load('../textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('../textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('../textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('../textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('../textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('../textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('../textures/door/roughness.jpg')
 
 
 //Bricks
